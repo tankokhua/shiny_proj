a4b40eee97c5b7bf9498d3d74ef12f1f668f384a
tankokhua <tankokhua@outlook.com>
2015-02-07 20:37:51 +0800

    - server.R, ui.R   . app to explore 'mtcars' dataset

--- server.R
+++ server.R
@@ -1,1 +1,33 @@
 # server.R
+library(UsingR)
+
+getformula <- function(x) {
+     if(length(x)==0) {
+        tmp <- "."
+     }
+     else {
+       tmp <- ""
+       for (i in seq_len(length(x)) ) {
+          tmp <- paste(tmp, x[i], sep="+")
+       }
+       tmp <- substring(tmp, 2)
+       tmp
+    }
+    tmp <- paste0("mpg ~ ", tmp)
+    tmp
+}
+
+shinyServer(
+    function(input, output) {
+      formula <- reactive({getformula({input$id1})})
+      output$oid1 <- renderPrint({formula()})
+      output$oid2 <- renderPrint({
+         lm({formula()}, data=mtcars)$coef
+      })
+      output$resPlot <- renderPlot({
+         fit <- lm({formula()}, data=mtcars)
+         par(mfrow=c(2,2))
+         plot(fit)
+      })
+    }
+)
--- ui.R
+++ ui.R
@@ -1,1 +1,22 @@
 # ui.R
+
+shinyUI (pageWithSidebar(
+    headerPanel("Exploratory Analysis of 'mtcars' dataset"),
+    sidebarPanel(
+       checkboxGroupInput("id1", "Regressors",
+             c("cyl" = "cyl", "disp" = "disp", "hp" = "hp", "drat" = "drat", 
+               "wt" = "wt",   "qsec" = "qsec", "vs" = "vs", "am"   = "am",
+               "gear" = "gear", "carb" = "carb")
+       )
+
+    ),
+    mainPanel(
+       h4("Linear Regression Formula:"),
+       verbatimTextOutput("oid1"),
+       h4("Coefficients:"),
+       verbatimTextOutput("oid2"),
+       h4("Residual Plots:"),
+       plotOutput("resPlot")
+    )
+))
+
